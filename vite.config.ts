afec756060b33a339fdb4c5b7360dfc8849ae1c1
Igor Gritsenko <agent190r@yandex.ru>
2025-07-16 13:24:42 +0300

    Refactor code structure for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
@@ -5,10 +6,11 @@ import { dirname, resolve } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/PlayableTools/',
   resolve: {
     alias: {
       fw: resolve(__dirname, 'src/fw'),
     },
   },
-});
+}));
