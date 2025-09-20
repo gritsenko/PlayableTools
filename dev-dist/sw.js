c74bdb36a609189b4f18a02a7f06e6dbd400416d
Igor Gritsenko <agent190r@yandex.ru>
2025-09-20 14:25:38 +0300

    Refactor code structure for improved readability and maintainability

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3jo6f9ekp"
+    "revision": "0.r593fl7rcgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
